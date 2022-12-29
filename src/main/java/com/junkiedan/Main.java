25b6ffcb50023af37d9b7a22012d8e31a2aea782
Stavros Barousis <stavrosbarousis@gmail.com>
2022-12-29 20:25:13 +0200

    First commit

---
+++ src/main/java/com/junkiedan/Main.java
@@ -0,0 +1,10 @@
+package com.junkiedan;
+
+import com.junkiedan.infinity.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+}
---
+++ src/main/java/com/junkiedan/infinity/Window.java
@@ -0,0 +1,94 @@
+package com.junkiedan.infinity;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+// This class is a singleton
+// It has a private constructor to ensure
+// that a window is only created from inside the class
+// Then on run function, when it is called, if the window
+// is null (not created yet) the function instantiates it
+// and returns it. If the window is already instantiated
+// then the active instance of the window is returned.
+public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Mario";
+    }
+
+    public static Window get() {
+        if(Window.window == null) {
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if(!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the Window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if(glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW Window");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable V-Sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while(!glfwWindowShouldClose(glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
